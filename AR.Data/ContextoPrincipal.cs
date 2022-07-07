ae0b55d9c6b3cc3f97bee44eec6b1e484f09c32a
Pamella Fernandes <pamellafernandes118@gmail.com>
2022-07-07 15:07:55 -0300

    Projeto Api Rest utilizando CSharp

---
+++ AR.Data/ContextoPrincipal.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AR.Data
+{
+    public class ContextoPrincípal : DbContext
+    {
+    }
+}
---
+++ AR.Domain/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    public class Cliente
+    {
+        [Key]
+        public Guid MyProperty { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O campo {0} dever no máximo {1} caracteres")]
+        public string Nome { get; set; }
+        public double CPF { get; set; }
+        public int Idade { get; set; }
+    }
+}
