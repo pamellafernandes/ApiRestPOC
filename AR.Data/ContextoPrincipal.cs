ba1f55d72b991160025a4aaf6456b0379d16d8cb
Pamella Fernandes <pamellafernandes118@gmail.com>
2022-07-10 22:32:02 -0300

    Projeto API Rest

--- AR.Data/ContextoPrincipal.cs
+++ AR.Data/ContextoPrincipal.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AR.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace AR.Data
 {
     public class ContextoPrincípal : DbContext
     {
+        public ContextoPrincípal(DbContextOptions options)
+             : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
     }
 }
